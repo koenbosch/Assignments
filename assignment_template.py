e454ed39cdfd79f8aaf851725af2ab1ac87a7748
koenbosch <42804314+koenbosch@users.noreply.github.com>
2019-01-03 14:38:10 +0100

    Add files via upload

---
+++ assignment_template.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# # Python assignment
+# 
+# The python assignment you can do either on your own or with one other student (i.e. max group size is 2 students).
+# 
+# The first cell of your notebook, should contain a table with the names and ANRs of the group members, like so
+# 
+# |Name|examnr.|
+# |----|-------|
+# |jan boone|12345|
+# |minke remmerswaal|56789|
+# 
+# 
+# 
+# See [the webpage](https://janboone.github.io/applied-economics/assignment3.html) for details of what we expect to see in this assignment.
+# 
+# 
+
+# # Importing packages
+
+# Here we import the packages we need for our project.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
